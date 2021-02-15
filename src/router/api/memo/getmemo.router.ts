7b2e08b1bb60306a4c3aa2f76ef2cbb2c50d753e
lukekeum <lukekeum11@gmail.com>
2021-02-15 22:47:06 +0900

    feat(router)!: Register memo get by id router
    This router will return memo by memo's id. I should add function
    that can get all memo, and get specific user's memo. After that,
    This project will be ended
    
    reporter: lukekeum

---
+++ src/router/api/memo/getmemo.router.ts
@@ -0,0 +1,35 @@
+import Memo from '@src/entity/memo.entity';
+import { FastifyPluginCallback } from 'fastify';
+
+interface IGetByIDParams {
+  id: string;
+}
+
+const getRoute: FastifyPluginCallback = (fastify, opts, done) => {
+  fastify.get('/get/:id', async (req, res) => {
+    const id = parseInt((req.params as IGetByIDParams)['id'], 10);
+
+    if (isNaN(id)) {
+      return res.status(401).send({ message: 'id should be number' });
+    }
+
+    try {
+      const memo = await Memo.findOne({ id });
+
+      if (!memo) {
+        return res.status(401).send({ message: 'Memo not found' });
+      }
+
+      return res.status(200).send({
+        data: { id: memo.id, content: memo.content, author_id: memo.user_id },
+      });
+    } catch (err) {
+      fastify.log.error(err);
+      return res.status(500).send({ error: err });
+    }
+  });
+
+  done();
+};
+
+export default getRoute;
--- src/router/api/memo/index.ts
+++ src/router/api/memo/index.ts
@@ -1,9 +1,11 @@
 import { FastifyPluginCallback } from 'fastify';
 
 import createRoute from './create.router';
+import getRoute from './getmemo.router';
 
 const memoRoute: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.register(createRoute, { prefix: '/' });
+  fastify.register(getRoute, { prefix: '/' });
 
   done();
 };
