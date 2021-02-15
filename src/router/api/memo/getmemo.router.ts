0b43e0aa0d77a483a9fca62c9dd8b3e697a746d5
lukekeum <lukekeum11@gmail.com>
2021-02-15 23:00:24 +0900

    feat(router)!: Add function that can get all memo by user
    This function can get memo by user's jwt token by header or 'id'
    parmeter. And this function will return memos that related with
    specific user.
    
    reporter: lukekeum

--- src/router/api/memo/getmemo.router.ts
+++ src/router/api/memo/getmemo.router.ts
@@ -5,7 +5,42 @@ interface IGetByIDParams {
   id: string;
 }
 
+interface IUserObjectType {
+  id: number;
+  userid: string;
+  nickname: string;
+  iat: number;
+}
+
 const getRoute: FastifyPluginCallback = (fastify, opts, done) => {
+  fastify.get('/user/:id', async (req, res) => {
+    const decodeJWT = (token: string) => {
+      const data = <IUserObjectType>(
+        Object.assign({}, fastify.jwt.decode(token))
+      );
+
+      if (!data || !data.id) {
+        res.status(401).send({ message: 'Invalid token' });
+        return -1;
+      }
+
+      return data.id;
+    };
+
+    const user_id =
+      parseInt((req.params as IGetByIDParams)['id'], 10) ||
+      decodeJWT(req.headers['x-auth-token'] as string);
+
+    try {
+      const memo = await Memo.find({ user_id });
+
+      return res.status(200).send({ data: memo });
+    } catch (err) {
+      fastify.log.error(err);
+      return res.status(500).send({ error: err });
+    }
+  });
+
   fastify.get('/get', async (req, res) => {
     try {
       const memo = await Memo.find();
